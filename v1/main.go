89361b2a6a0e3f6fe21bd71a43b346dcf728e15c
kwakubiney <kebiney@hotmail.com>
2023-09-24 21:44:43 +0000

    feat: add nginx gateway to route ingress traffic to pods

--- v1/main.go
+++ v1/main.go
@@ -5,20 +5,11 @@ import (
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello\n")
-}
-
-func headers(w http.ResponseWriter, req *http.Request) {
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
-	}
+func hi(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hi, I am v1\n")
 }
 
 func main() {
-	http.HandleFunc("/", hello)
-	http.HandleFunc("/headers", headers)
-	http.ListenAndServe(":8000", nil)
+	http.HandleFunc("/v1", hi)
+	http.ListenAndServe(":8080", nil)
 }
--- v2/main.go
+++ v2/main.go
@@ -6,19 +6,10 @@ import (
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hi\n")
-}
-
-func headers(w http.ResponseWriter, req *http.Request) {
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
-		}
-	}
+	fmt.Fprintf(w, "hello i am v2\n")
 }
 
 func main() {
-	http.HandleFunc("/", hello)
-	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/v2", hello)
 	http.ListenAndServe(":8080", nil)
 }
